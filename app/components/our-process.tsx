171e745f13039235621af9841215cd38fe6ff670
Estrella-BF <gracielabenitesf@gmail.com>
2026-01-07 00:24:43 -0500

    fix: our process

--- app/components/our-process.tsx
+++ app/components/our-process.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Box from "@mui/material/Box";
 
 export const OurProcess = () => {
@@ -8,6 +9,40 @@ export const OurProcess = () => {
       <h3 className="leading-none mb-10 mt-4">
         Los pasos para tu <br /> lanzamiento espacial
       </h3>
+      <div className="our-process__steps mb-4">
+        <figure className="py-4">
+          <Image
+            alt="software-developer"
+            className="w-auto h-auto"
+            src="/images/process-initial.png"
+            height={1000}
+            width={1000}
+          />
+        </figure>
+      </div>
+      <div className="our-process__steps mb-4">
+        <figure className="py-4">
+          <Image
+            alt="software-developer"
+            className="w-auto h-auto"
+            src="/images/process-agile.png"
+            height={1000}
+            width={1000}
+          />
+        </figure>
+      </div>
+      <div className="our-process__steps mb-4">
+        <figure className="py-4">
+          <Image
+            alt="software-developer"
+            className="w-auto h-auto"
+            src="/images/process-hybrid.png"
+            height={1000}
+            width={1000}
+          />
+        </figure>
+      </div>
+
     </Box>
   )
 };
