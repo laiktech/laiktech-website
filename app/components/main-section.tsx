8fba7f17d68e385e9424c477c1259784db00d77d
Estrella-BF <gracielabenitesf@gmail.com>
2026-01-10 00:07:43 -0500

    main section

--- app/components/main-section.tsx
+++ app/components/main-section.tsx
@@ -2,16 +2,14 @@ import { forwardRef } from "react";
 import Image from "next/image";
 import Box from "@mui/material/Box";
 import { CustomButton } from "../shared/components/custom-button";
+import { LAIKTECH_PHONE, WHATSAPP_BASIC_MESSAGE } from "../shared/components/const/whatsapp";
 
 const titleStyle = "text-white text-[4rem] font-black leading-[1.2]";
 
 export const MainSection = forwardRef<HTMLDivElement>(function Banner(_props, ref) {
   const handleContactButton = () => {
-  const laiktechPhone = "51993230963";
-  const basepath = `https://api.whatsapp.com`;
-  const message = `Hola%2C%20Equipo%20de%20LaikTech%20%F0%9F%91%8B.%20Estoy%20contact%C3%A1ndote%20por%20la%20pagina%20web%20de%20Laiktech%20y%20me%20gustar%C3%ADa%20conversar%20con%20ustedes.`;
-
-  const url = `${basepath}/send?phone=${laiktechPhone}&text=${message}`;
+    const basepath = `https://api.whatsapp.com`;
+    const url = `${ basepath }/send?phone=${ LAIKTECH_PHONE }&text=${ WHATSAPP_BASIC_MESSAGE }`;
     window.open(url);
   };
 
---
+++ app/shared/components/const/whatsapp.tsx
@@ -0,0 +1,2 @@
+export const LAIKTECH_PHONE = "51993230963";
+export const WHATSAPP_BASIC_MESSAGE = `Hola%2C%20Equipo%20de%20LaikTech%20%F0%9F%91%8B.%20Estoy%20contact%C3%A1ndote%20por%20la%20pagina%20web%20de%20Laiktech%20y%20me%20gustar%C3%ADa%20conversar%20con%20ustedes.`;
