b8b0ee672f9dea8e34410d293ac11efd91fbc0f1
Estrella-BF <gracielabenitesf@gmail.com>
2026-01-27 14:15:33 -0500

    feat: our process animation

--- app/components/our-process.tsx
+++ app/components/our-process.tsx
@@ -9,7 +9,10 @@ const counterStyle = "num inline-block";
 export const OurProcess = () => {
   const componentRef = useRef<HTMLDivElement | null>(null);
   const countdownRef = useRef<HTMLParagraphElement | null>(null);
-  
+  const step1FigureRef = useRef<HTMLElement | null>(null);
+  const step2FigureRef = useRef<HTMLElement | null>(null);
+  const step3FigureRef = useRef<HTMLElement | null>(null);
+
   useEffect(() => {
     if (!componentRef.current || !countdownRef.current) return;
     const numbers = countdownRef.current.querySelectorAll<HTMLSpanElement>(".num");
@@ -21,6 +24,30 @@ export const OurProcess = () => {
           easing: "ease-out",
           duration: 700,
           delay: stagger(1000),
+        });
+
+      if (step1FigureRef.current)
+        animate(step1FigureRef.current, {
+          translateX: ['0vw', '-100vw'],
+          easing: "ease-out",
+          duration: 700,
+          delay: 0,
+        });
+
+       if (step2FigureRef.current)
+        animate(step2FigureRef.current, {
+          translateX: ['0vw', '100vw'],
+          easing: "ease-out",
+          duration: 700,
+          delay: 800,
+        });
+
+       if (step3FigureRef.current)
+        animate(step3FigureRef.current, {
+          translateX: ['0vw', '-100vw'],
+          easing: "ease-out",
+          duration: 700,
+          delay: 1600,
         });
       }
     }, { threshold: 0 });
@@ -37,7 +64,7 @@ export const OurProcess = () => {
         Los pasos para tu <br /> lanzamiento espacial
       </h3>
       <div className="our-process__steps mb-4">
-        <figure className="py-4">
+        <figure ref={ step1FigureRef } className="py-4 transform translate-x-[100vw]">
           <Image
             alt="software-developer"
             className="w-auto h-auto"
@@ -48,7 +75,7 @@ export const OurProcess = () => {
         </figure>
       </div>
       <div className="our-process__steps mb-4">
-        <figure className="py-4">
+        <figure ref={ step2FigureRef } className="py-4 transform translate-x-[-100vw]">
           <Image
             alt="software-developer"
             className="w-auto h-auto"
@@ -59,7 +86,7 @@ export const OurProcess = () => {
         </figure>
       </div>
       <div className="our-process__steps mb-4">
-        <figure className="py-4">
+        <figure ref={ step3FigureRef } className="py-4 transform translate-x-[100vw]">
           <Image
             alt="software-developer"
             className="w-auto h-auto"
