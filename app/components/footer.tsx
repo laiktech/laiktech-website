9d23fbbe32ff4f32f5f626992f2d00503f5657f9
Estrella-BF <gracielabenitesf@gmail.com>
2026-01-27 16:45:07 -0500

    fix: footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -27,13 +27,21 @@ export const Footer = () => {
           height={50}
         />
       </div>
-      <div>
+      <div className="grid grid-flow-col auto-cols-max gap-4 items-center">
         <a href="mailto:contacto@laiktech.com">
           <Image
             src="/icons/email.png"
             alt="logo"
-            width={40}
+            width={46}
             height={40}
+          />
+        </a>
+        <a href="mailto:contacto@laiktech.com">
+          <Image
+            src="/icons/chat.png"
+            alt="logo"
+            width={40}
+            height={55}
           />
         </a>
       </div>
